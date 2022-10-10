8b479af57aa6f076e692db439c21f8a97d0d14cd
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-10-10 13:42:52 -0400

    loading lazy component (lazy2) with children components (lazy2a, lazy2b)

--- src/app/lazy2.component.ts
+++ src/app/lazy2.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit, ViewContainerRef } from '@angular/core';
+import { Lazy2aComponent } from './lazy2a.component';
+import { Lazy2bComponent } from './lazy2b.component';
 
 @Component({
   template: `
     <p>
-      lazy2 works!
+      lazy2 component loaded
     </p>
   `,
   styles: [
@@ -11,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Lazy2Component implements OnInit {
 
-  constructor() { }
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    private cfr: ComponentFactoryResolver
+  ) { }
 
   ngOnInit(): void {
+    // * NOTE: loading child component (lazy2a) of lazy2 component
+    const componentFactoryA = this.cfr.resolveComponentFactory(Lazy2aComponent);
+    this.viewContainerRef.createComponent(componentFactoryA);
+
+    // * NOTE: loading child component (lazy2b) of lazy2 component
+    const componentFactoryB = this.cfr.resolveComponentFactory(Lazy2bComponent);
+    this.viewContainerRef.createComponent(componentFactoryB);
   }
 
 }
