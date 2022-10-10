08f5721b201e146435f1f382c2fd1b3e2bd99b1b
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-10-10 13:36:15 -0400

    add lazy components (lazy1, lazy2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+  <div>
+    <div>Hello Lazy Loading Component Example!! </div>
+    <title>{{ title }}</title>
+    <button (click)="getLazy1()">Lazy Component 1</button>
+    <button (click)="getLazy2()">Lazy Component 2</button>
+  </div>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'lazy-demo';
+  title = 'lazy-component';
+  
+  constructor(
+    private viewContainerRef: ViewContainerRef,
+    // ! FIXME
+    private cfr: ComponentFactoryResolver
+  ){}
+  
+
+  async getLazy1(){
+    this.viewContainerRef.clear();
+    const { Lazy1Component } = await import('./lazy1.component');
+    // ! FIXME
+    this.viewContainerRef.createComponent(
+      this.cfr.resolveComponentFactory(Lazy1Component)
+    );
+  }
+
+  async getLazy2(){
+    this.viewContainerRef.clear();
+    const { Lazy2Component } = await import('./lazy2.component');
+    // ! FIXME
+    this.viewContainerRef.createComponent(
+      this.cfr.resolveComponentFactory(Lazy2Component)
+    );
+  }
 }
